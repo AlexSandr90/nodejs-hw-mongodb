c0f95239e99c597a6c566e1e896410d28d7c523f
Oleksandr <gayrsuk@gmail.com>
2024-08-05 18:30:20 +0300

    added new features
    Added Sorting, validation ID, pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,9 +6,14 @@ import {
   updateContactById,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
-export const getAllContactsController = async (req, res, next) => {
-  const contacts = await getAllContacts();
+export const getContactsController = async (req, res, next) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
+  const contacts = await getAllContacts({ page, perPage, sortBy, sortOrder });
 
   if (!contacts) {
     next(createHttpError(404, 'Failed to get contacts'));
